883b0af1b219029f40a42b9e1c7d3135a95b9c9c
CarlosAMF02 <c.a.mascarenhas@hotmail.com>
2021-09-04 20:51:54 -0300

    exmplos da aula1

---
+++ aula1/aula1Boolean.js
@@ -0,0 +1,8 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero); //primeiro numero é igual ao segundo numero?
+
+const texto1 = "Alura";
+const texto2 = "alura";
+console.log(texto1 === texto2); //Da false pq a letra maiuscula diferencia!
---
+++ aula1/aula1String.js
@@ -0,0 +1,20 @@
+const texto1 = "Olá Mundo!";
+
+//concatenacao
+const citacao = "Meu nome é ";
+const nome = "Carlos";
+console.log(citacao + nome)
+
+
+
+//Template Srting ou Template Literal - pode colocar a variavel dentro
+const texto2 = `Ola ${texto1}`;
+console.log(texto2);
+
+// /u quer dizer unicode 
+
+const texto3 = texto1.toLowerCase();
+console.log(texto3);
+const senha = "1234";
+console.log(senha.length) //tamanho
+
