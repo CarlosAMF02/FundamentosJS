710c609862d80f7f1944a243e46ff5ad442fd27b
CarlosAMF02 <c.a.mascarenhas@hotmail.com>
2021-09-05 12:00:33 -0300

    codigo var e conversoes

---
+++ aula2/aula2Boolean.js
@@ -0,0 +1,23 @@
+// boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// 0 => false;
+//1 => true;
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+//null => vazio ou nada
+//undefined 
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull); //tem como resultado o tipo objeto!!
---
+++ aula2/conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dado
+// booleanos
+
+//conversao implicita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero+numeroString); //converte numero para String e faz uma concatenacao
+
+//conversao explicita
+//Number()
+//String()
+console.log(numero + Number(numeroString)) //n pode ter nenhum caractere que nao seja um numero, pois tera como resultado NaN
+
+
+
